29394e20ad514d959d14afeb3dbbe138a4d2f001
Ramsey,Arron B <abramsey@freemanhealth.net>
2022-11-03 15:53:10 -0500

    Moving / modifying TwitterApiService

---
+++ Infrastructure/Services/Implementations/TwitterApiService.cs
@@ -0,0 +1,33 @@
+﻿using Core.Interfaces;
+using Infrastructure.Services.Interfaces;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Streaming.V2;
+
+namespace Infrastructure.Services.Implementations
+{
+    public class TwitterApiService : ITwitterApiService
+    {
+        private ISettingService settingService { get; }
+
+        public TwitterApiService(ISettingService settings)
+        {
+            settingService = settings;
+        }
+
+        public ISampleStreamV2 GetStream()
+        {
+            return new TwitterClient(GetCredentials()).StreamsV2.CreateSampleStream();
+        }
+
+        private ConsumerOnlyCredentials GetCredentials()
+        {
+            return new ConsumerOnlyCredentials()
+            {
+                BearerToken = settingService.GetSettings().BearerToken,
+                ConsumerKey = settingService.GetSettings().ConsumerKey,
+                ConsumerSecret = settingService.GetSettings().ConsumerSecret
+            };
+        }
+    }
+}
---
+++ Infrastructure/Services/Interfaces/ITwitterApiService.cs
@@ -0,0 +1,10 @@
+﻿
+using Tweetinvi.Streaming.V2;
+
+namespace Infrastructure.Services.Interfaces
+{
+    public interface ITwitterApiService
+    {
+        ISampleStreamV2 GetStream();
+    }
+}
