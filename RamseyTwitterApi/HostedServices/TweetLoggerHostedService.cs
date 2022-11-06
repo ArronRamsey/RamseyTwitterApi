e7e386199ea16cb766284778ed1a8d9340c3e3bd
Ramsey,Arron B <abramsey@freemanhealth.net>
2022-11-06 11:25:35 -0600

    Added TweetLoggerHostedService and tests

---
+++ RamseyTwitterApi/HostedServices/TweetLoggerHostedService.cs
@@ -0,0 +1,27 @@
+﻿using Core.Services.Interfaces;
+
+namespace RamseyTwitterApi.HostedServices
+{
+    public class TweetLoggerHostedService : IHostedService
+    {
+        private ITweetService TweetService { get; }
+
+        public TweetLoggerHostedService(ITweetService tweetService)
+        {
+            TweetService = tweetService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            TweetService.StartWriteLogAsync();
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            TweetService.StopWriteLogAsync();
+            return Task.CompletedTask;
+        }
+       
+    }
+}
