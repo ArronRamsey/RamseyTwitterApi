a14737b042828c37e49a3000a2258fb884784e3a
Ramsey,Arron B <abramsey@freemanhealth.net>
2022-11-06 14:11:35 -0600

    Returning dto in tweetscontroller

---
+++ Core/Dtos/TweetInformationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Dtos
+{
+    public class TweetInformationDto
+    {
+        public int TweetsReceived { get; set; }
+        public double TweetsPerMinute { get; set; }
+    }
+}
---
+++ RamseyTwitterApi/Controllers/TweetsController.cs
@@ -0,0 +1,28 @@
+﻿using Core.Dtos;
+using Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RamseyTwitterApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TweetsController : ControllerBase
+    {
+        private ITweetService apiService { get; }
+        
+        public TweetsController(ITweetService tweetService)
+        {
+            apiService = tweetService;
+        }
+
+        [HttpGet]
+        public TweetInformationDto GetInfo()
+        {
+            return new TweetInformationDto()
+            {
+                TweetsPerMinute = apiService.TweetsPerMinute,
+                TweetsReceived = apiService.TweetCount
+            };
+        }
+    }
+}
