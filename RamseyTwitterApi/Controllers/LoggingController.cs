c59056e3d42bedb62452f23984fbad5fbfc971b2
Ramsey,Arron B <abramsey@freemanhealth.net>
2022-11-06 14:36:27 -0600

    Added controllers to change values

---
+++ RamseyTwitterApi/Controllers/LoggingController.cs
@@ -0,0 +1,31 @@
+﻿using Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RamseyTwitterApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoggingController : ControllerBase
+    {
+        private ITweetService TweetService { get; }
+        
+        public LoggingController(ITweetService tweetService)
+        {
+            TweetService = tweetService;
+        }
+
+        [HttpPut]
+        [Route("StopLogging")]
+        public void StopLogging()
+        {
+            TweetService.StopWriteLogAsync();
+        }
+
+        [HttpPut]
+        [Route("StartLogging")]
+        public void StartLogging()
+        {
+            TweetService.StartWriteLogAsync();
+        }
+    }
+}
---
+++ RamseyTwitterApi/Controllers/TwitterStreamController.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RamseyTwitterApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TwitterStreamController : ControllerBase
+    {
+        private ITwitterApiService ApiService { get; }
+        
+        public TwitterStreamController(ITwitterApiService service)
+        {
+            ApiService = service;
+        }
+
+        [HttpPut]
+        [Route("Connect")]
+        public void Connect()
+        {
+            ApiService.Connect();
+        }
+
+        [HttpPut]
+        [Route("Disconnect")]
+        public void Disconnect()
+        {
+            ApiService.Disconnect();
+        }
+    }
+}
