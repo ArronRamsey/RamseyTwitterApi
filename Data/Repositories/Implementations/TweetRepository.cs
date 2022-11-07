42ba35195bfc70b6721a89501034b4425a8eaba1
Ramsey,Arron B <abramsey@freemanhealth.net>
2022-11-06 19:27:11 -0600

    Added Tweet Repositories

---
+++ Data/Repositories/Implementations/TweetRepository.cs
@@ -0,0 +1,20 @@
+﻿using Data.Entities;
+using Data.Repositories.Interfaces;
+
+namespace Data.Repositories.Implementations
+{
+    public class TweetRepository : ITweetRepository
+    {
+        private List<Tweets> Tweets { get; set; } = new List<Tweets>();
+
+        public IEnumerable<Tweets> GetAll()
+        {
+            return Tweets.AsEnumerable();
+        }
+
+        public void SaveTweet(Tweets tweet)
+        {
+            Tweets.Add(tweet);
+        }
+    }
+}
---
+++ Data/Repositories/Interfaces/ITweetRepository.cs
@@ -0,0 +1,10 @@
+﻿using Data.Entities;
+
+namespace Data.Repositories.Interfaces
+{
+    public interface ITweetRepository
+    {
+        IEnumerable<Tweets> GetAll();
+        void SaveTweet(Tweets tweet);
+    }
+}
