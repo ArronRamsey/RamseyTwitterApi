41dd993de496644aad101ad9fd8a48b4c166760e
Ramsey,Arron B <abramsey@freemanhealth.net>
2022-11-08 12:14:43 -0600

    Refactored Properties to methods

--- Core/Services/Interfaces/ITweetStatisticsService.cs
+++ Core/Services/Interfaces/ITweetStatisticsService.cs
@@ -1,17 +1,12 @@
 ﻿using Core.Dtos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Services.Interfaces
 {
     public interface ITweetStatisticsService
     {
-        int TweetCount { get; }
-        double TweetsPerMinute { get; }
-        TweetStatisticsDto Statistics { get; }
+        int GetTweetCount();
+        double GetTweetsPerMinute();
+        TweetStatisticsDto GetStatistics();
         void TweetReceived();
 
     }
