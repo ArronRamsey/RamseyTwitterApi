b818e66872670e80de932b2fe4c35fcc6a9bd862
Ramsey,Arron B <abramsey@freemanhealth.net>
2022-11-08 05:10:03 -0600

    Added Hashtag Entity

---
+++ Entities/HashTagEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http.Headers;
+
+namespace Entities
+{
+    public class HashTagEntity
+    {
+        public string Id { get; set; }  = string.Empty;
+        public string TweetId { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+        public IEnumerable<HashTagEntity> Tags { get; set; } = new List<HashTagEntity>();
+    }
+}
