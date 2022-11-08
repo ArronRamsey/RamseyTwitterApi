bc830989cca1bf5a5fc1f5aff8a0ea9dac6dfb45
Ramsey,Arron B <abramsey@freemanhealth.net>
2022-11-08 06:11:11 -0600

    changed ID property to string, used IList at type for Tags Property.

--- Entities/TweetEntity.cs
+++ Entities/TweetEntity.cs
@@ -2,11 +2,11 @@
 {
     public class TweetEntity
     {
-        public int Id { get; set; }
+        public string Id { get; set; } = string.Empty;
         public string Text { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; }
-        public IEnumerable<HashTagEntity> Tags { get; set; } = new List<HashTagEntity>();    
+        public IList<HashTagEntity> Tags { get; set; } = new List<HashTagEntity>();    
     }
 
 }
