4893715a5cf4f360013f44e37b5a9301ddaec2d5
Ramsey,Arron B <abramsey@freemanhealth.net>
2022-11-08 06:09:54 -0600

    Removed overloaded constructor, added HashTag collection

--- Core/Dtos/TweetDto.cs
+++ Core/Dtos/TweetDto.cs
@@ -5,16 +5,10 @@
         public string Text { get; set; } = string.Empty;
         public string AuthorId { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; }
+        public IEnumerable<string> HashTags { get; set; } = new List<string>();
 
         public TweetDto()
         {
-        }
-
-        public TweetDto(string TweetText, string TweetAuthorId, DateTime DateCreated)
-        {
-            Text = TweetText;
-            AuthorId = TweetAuthorId;
-            CreatedOn = DateCreated;    
         }
 
         public override bool Equals(object? obj)
@@ -27,6 +21,7 @@
             return other is not null &&
                    Text == other.Text &&
                    CreatedOn == other.CreatedOn &&
+                   HashTags.SequenceEqual(other.HashTags) &&
                    AuthorId == other.AuthorId;
         }
 
