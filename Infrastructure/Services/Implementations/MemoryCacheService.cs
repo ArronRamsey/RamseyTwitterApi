ecb94984643da73b7a6c143df1024e553583d399
Ramsey,Arron B <abramsey@freemanhealth.net>
2022-11-07 18:52:06 -0600

    Added MemoryCacheService

---
+++ Infrastructure/Services/Implementations/MemoryCacheService.cs
@@ -0,0 +1,50 @@
+﻿using Infrastructure.Services.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Infrastructure.Services.Implementations
+{
+    public class MemoryCacheService : ICache
+    {
+        private IMemoryCache MemoryCache { get; set; }
+        
+        private const int DefaultCacheLengthInMinutes = 60;
+
+        public MemoryCacheService(IMemoryCache cache)
+        {
+            MemoryCache = cache;
+        }
+
+        public void Set(string key, object value)
+        {
+            MemoryCache.Remove(key);
+            if (value != null)
+            {
+                Set(key, value, DefaultCacheLengthInMinutes);
+            }
+        }
+
+        public void Set(string key, object value, int lengthInMinutes)
+        {
+            MemoryCache.Remove(key);
+            if (value != null)
+            {
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(DefaultCacheLengthInMinutes));
+                MemoryCache.Set(key, value, cacheEntryOptions);
+            }
+        }
+
+        public object Get(string key)
+        {
+            var entry = MemoryCache.Get(key);
+            return entry;
+        }
+
+        public T Get<T>(string key)
+        {
+            var entry = MemoryCache.Get<T>(key);
+            return entry;
+        }
+
+    }
+}
+
---
+++ Infrastructure/Services/Interfaces/ICache.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Services.Interfaces
+{
+    public interface ICache
+    {
+        void Set(string key, object value);
+        void Set(string key, object value, int lengthInMinutes);
+        object Get(string key);
+        T Get<T>(string key);
+    }
+}
