171c5eacb5f8162ccbc515d676efd77894ee8c5b
Ramsey,Arron B <abramsey@freemanhealth.net>
2022-11-08 05:11:20 -0600

    Added HashTagEntity collection

--- Entities/TweetEntity.cs
+++ Entities/TweetEntity.cs
@@ -6,6 +6,7 @@
         public string Text { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; }
+        public IEnumerable<HashTagEntity> Tags { get; set; } = new List<HashTagEntity>();    
     }
 
 }
